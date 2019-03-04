312b926ee3da109054c36faeaa1c972c521be8e2
Sachin Mahajan <sachinm78@gmail.com>
2019-03-04 18:34:21 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -94,10 +94,7 @@ def changing_alan
           :languages => ["C"]
         }
      }
-     #change what Alan Kay is :known_for to the value of the alans_new_info variable. 
-     alans_new_info = "GUI"
-     
-     
+  alans_new_info = "GUI"
 end
 
 def adding_to_dennis
